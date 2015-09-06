689715ad9c6eddd69a5136fa0f7ba0add0638955
Corey Gibson <cgibsonmm@gmail.com>
2015-09-05 22:53:30 -0400

    wrote more test models need to finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { in: 4..55 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new( name: "Example User", email: "user@example.com", zip_code: "12747")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "user name should be presnent" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "user name should be at least 3 char" do
+    @user.name = "aa"
+    assert_not @user.valid?
+  end
+
+  test "user name should be less then 55 char" do
+    @user.name = "a" * 56
+    assert_not @user.valid?
+  end
+# Finish tests 
+  test "email should be pressent" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+
 end
