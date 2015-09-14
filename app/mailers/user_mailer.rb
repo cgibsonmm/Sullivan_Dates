e324c51c84be611d9d5a46ff8bc729a1e413b491
Corey Gibson <cgibsonmm@gmail.com>
2015-09-13 23:27:35 -0400

    rough mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'CGibson@sulivandates.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'https://www.facebook.com/groups/812762058783473/'
+    mail(to: @user.email, subject: 'Thank you for signing up for Sullivan Dates notifications')
+  end
 end
