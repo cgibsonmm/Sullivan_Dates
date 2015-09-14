d974c9f46ceaafb3f9357d94d5dbf1ad83fb05f6
Corey Gibson <cgibsonmm@gmail.com>
2015-09-14 07:41:15 -0400

    edit mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "CGibson@SullivanDates.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'CGibson@sulivandates.com'
+  default from: 'CGibson@SullivanDates.com'
 
   def welcome_email(user)
     @user = user
     @url = 'https://www.facebook.com/groups/812762058783473/'
-    mail(to: @user.email, subject: 'Thank you for signing up for Sullivan Dates notifications')
+    mail(to: @user.email, subject: 'Sullivan Dates | Coming soon')
   end
 end
